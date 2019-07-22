70269e474f91125ea2f88fdc5674a7c0b059c944
vvshinevv <vvshinevv@tmon.co.kr>
2019-07-23 00:12:41 +0900

    17-3 컴퓨턴트 이벤트 만들기까지

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "read",
+      selected_content_id: 2,
+      subject: { title: "WEB", sub: "world wide web!" },
+      welcome: { title: "Welcome", desc: "Hello, React" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" }
+      ]
+    };
+  }
+
+  render() {
+    console.log("App render");
+    var _title,
+      _desc = null;
+
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.tile;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+    }
+
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function() {
+            this.setState({ mode: "welcome" });
+          }.bind(this)}
+        />
+        <TOC
+          onChangePage={function(id) {
+            this.setState({ mode: "read", selected_content_id: Number(id) });
+          }.bind(this)}
+          data={this.state.contents}
+        />
+        <Content title={_title} desc={_desc} />
+      </div>
+    );
+  }
 }
 
 export default App;
